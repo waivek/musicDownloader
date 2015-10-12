c510a58e2e204379fd6c8c7970d816c73e5cab50
waivek <waivek@gmail.com>
2015-10-13 03:34:51 +0530

    Precautionary commit before test-main merge

--- test.js
+++ test.js
@@ -1,16 +1,5 @@
-// id3 -a "Wonder Girls" -t "One Black night" -g <genre number>
-// id3 -f "%a-%t" song.mp3
-
 var casper_print = require('./helper').casper_print;
 var extract_text = require('./helper').extract_text;
-var extract_artist_and_title = function ( str ) {
-};
-// var extract_text = function ( str, beg, end ) {
-//     var begIndex = str.indexOf(beg);
-//     var endIndex = str.indexOf(end, begIndex+1);
-//     var res = str.substring(begIndex+1, endIndex);
-//     return res;
-// };
 
 var casper = require('casper').create();
 var x = require('casper').selectXPath;
@@ -22,31 +11,44 @@ casper.start(bad_url, function() {
 }).viewport(1200, 1000);
 
 
-var contains = function (  ) {
-    // enter code here
-};
 casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
         var artist = "unknown_artist";
         var title = "unknown_title";
         var str = "unknown_str";
         var x_path_itunes = x('//*[@id="watch-description-extras"]/ul/li[1]/ul/li/a');
         url = this.getElementAttribute( x('//*[@id="watch-description-extras"]/ul/li[1]/ul/li/a'), 'href' );
+
+        // checking if YouTube has tagged the song and linked to the
+        // corresponding iTunes page
         if (url.indexOf ( 'itunes' ) > -1 ) {
-            console.log("Area Exists!");
+            // The format of the text will be :
+            // str = "$title" by $artist" (
+            // EXAMPLE : 
             // str = "One Black Night" by Wonder Girls (
             str = casper.fetchText(x_path_itunes);
-            console.log("str : " + str);
             title = extract_text( str, '"', '"');
             artist = extract_text( str, '" by ', ' (');
-            casper_print(this);
         }  else {
+
+            // this means that YouTube has NOT tagged the song
+            // we need to manually search for the iTunes link
+            url = "unknown_url";
             str = casper.fetchText(x('//*[@id="eow-title"]'));
-            var index_dash = str.lastIndexOf('-');
-            artist = str.substring(0, index_dash);
-            title = str.substring(index_dash+1).trim();
+
+            // This block tries to extract $artist and $title from the title of
+            // the YouTube video. It is VERY unreliable. It would be safer to
+            // later add an option asking the user whether the $title and the
+            // $artist are correct or not, before initiating a search for the
+            // URL. If the title is of the for a-b-c-d then $artist = a-b-c and
+            // $title = $d
+            // var index_dash = str.lastIndexOf('-');
+            // artist = str.substring(0, index_dash).trim();
+            // title = str.substring(index_dash+1).trim();
+
+            
         }
         console.log("title : " + title);
-        console.log("artist : " + artist);
+        console.log("artist : " + artist );
         console.log("url : " + url);
 });
 
