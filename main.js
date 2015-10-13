c985cba1dfdee3f2dca796d8eefc52d94d81f6bc
waivek <waivek@gmail.com>
2015-10-13 17:00:21 +0530

    Major refactoring
    Removed a lot of legacy comments. Might need to look back at the legacy
    comments if my this.click() method begins to fail for some reason. Also
    removed a lot of helper functions which became redundant by using
    this.click()

--- main.js
+++ main.js
@@ -45,17 +45,6 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
             url_itunes = "unknown_url";
             str = casper.fetchText(x('//*[@id="eow-title"]'));
 
-            // This block tries to extract $artist and $title from the title of
-            // the YouTube video. It is VERY unreliable. It would be safer to
-            // later add an option asking the user whether the $title and the
-            // $artist are correct or not, before initiating a search for the
-            // URL. If the title is of the for a-b-c-d then $artist = a-b-c and
-            // $title = $d
-            // var index_dash = str.lastIndexOf('-');
-            // artist = str.substring(0, index_dash).trim();
-            // title = str.substring(index_dash+1).trim();
-
-            // TODO: Search img_url by Image
             var getUrl = function (name) {
                 name = name.trim();
                 first = "https://www.google.co.in/search?q=";
@@ -67,53 +56,13 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
             this.thenOpen(getUrl( str ), function () {
                 console.log("Now opening " + getUrl( str ));
             });
-
-            var href2url = function (href) {
-                var url = extract_text(href, '=', '&');
-                return url;
-            };
-
-            // var getLinks = function () {
-            //     // TODO: Use querySelector
-            //     var links = document.querySelectorAll('h3.r a');
-            //     return Array.prototype.map.call(links, function (e) {
-            //         return e.getAttribute('href');
-            //     });
-            // };
-
-            // to be run after google-search
             // TODO: Your search did not match any results
-            //
-            // NOTE: Browser-dependent selector
-            // The selector for the first link in Chrome is -
-            // //*[@id="rso"]/div[1]/div[1]/div/h3/a
-            // The selector for the first link in PhantomJS is -
-            // //*[@id="ires"]/ol/li[1]/h3/a
-
-            this.waitForSelector ( x('//*[@id="ires"]/ol/li[1]/h3/a'), function () {
-                console.log("Completed google search");
-                this.capture('google.png');
-
-                // var url_list = this.evaluate(getLinks);
-                // url_itunes = href2url(url_list[0]);
-
-                // url_itunes = this.getElementAttribute( x('/#<{(|[@id="ires"]/ol/li[1]/h3/a'), 'href' );
-                // url_itunes = href2url(url_itunes);
-                
-                // this.click(x('/#<{(|[@id="ires"]/ol/li[1]/h3/a'));
+            this.waitForSelector ( 'h3.r a', function () {
                 this.click('h3.r a');
             });
-
         }
-        //
-        // this.thenOpen(url_itunes, function() {
-        //     console.log("Now opening url_itunes = " + url_itunes);
-        // });
 });
 
-// run after iTunes
-// CHROME xPath = 
-//                          //*[@id="left-stack"]/div[1]/ul/li[3]/span[2]
 casper.waitForSelector ( x('//*[@id="left-stack"]/div[1]/ul/li[3]/span[2]'), function () {
 
     console.log("Loaded iTunes page");
