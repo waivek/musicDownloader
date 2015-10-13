c13bce98f2aa07f71f78bc7e84011b8a039f3983
waivek <waivek@gmail.com>
2015-10-13 16:52:08 +0530

    Works for url and bad_url
    Insted of having a common thenOpen after the if-else, I have a thenOpen
    only in the if. In the else I have put a this.click() which basically
    clicks the first link in the google-search. For some reason, after
    getting the iTunes url in the else, the original common thenOpen was NOT
    opening the corresponding iTunes page. Also, this.click() seems to be
    cleaner and more natural

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var url = "https://www.youtube.com/watch?v=U5K9AlmWM8s";
 var bad_url = "https://www.youtube.com/watch?v=I2REZSj4XnE";
 
 casper.start(bad_url, function() {
+// casper.start(url, function() {
     this.echo(this.getTitle());
 }).viewport(1200, 1000);
 
@@ -19,22 +20,31 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
 
         // checking if YouTube has tagged the song and linked to the
         // corresponding iTunes page
+
         if (url_itunes.indexOf ( 'itunes' ) > -1 ) {
             console.log("iTunes link exists");
+
             // The format of the text will be :
             // str = "$title" by $artist" (
             // EXAMPLE : 
             // str = "One Black Night" by Wonder Girls (
+
             str = casper.fetchText(x_path_itunes);
             title = extract_text( str, '"', '"');
             artist = extract_text( str, '" by ', ' (');
+
+            this.thenOpen(url_itunes, function() {
+                console.log("Now opening url_itunes = " + url_itunes);
+            });
         }  else {
             console.log("iTunes link not found.");
 
             // this means that YouTube has NOT tagged the song
             // we need to manually search for the iTunes link
+
             url_itunes = "unknown_url";
             str = casper.fetchText(x('//*[@id="eow-title"]'));
+
             // This block tries to extract $artist and $title from the title of
             // the YouTube video. It is VERY unreliable. It would be safer to
             // later add an option asking the user whether the $title and the
@@ -53,61 +63,79 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
                 end = "+site%3Aitunes.apple.com";
                 return first + mid + end;
             };
-            console.log("Searching Google... for " + getUrl( str ));
-            this.thenOpen(getUrl( str ));
-            // this.wait(5000, function () {
-            //     console.log("Finished waiting 5 seconds");
-            // });
+
+            this.thenOpen(getUrl( str ), function () {
+                console.log("Now opening " + getUrl( str ));
+            });
 
             var href2url = function (href) {
                 var url = extract_text(href, '=', '&');
                 return url;
             };
 
-            var getLinks = function () {
-                // TODO: Use querySelector
-                var links = document.querySelectorAll('h3.r a');
-                return Array.prototype.map.call(links, function (e) {
-                    return e.getAttribute('href');
-                });
-            };
+            // var getLinks = function () {
+            //     // TODO: Use querySelector
+            //     var links = document.querySelectorAll('h3.r a');
+            //     return Array.prototype.map.call(links, function (e) {
+            //         return e.getAttribute('href');
+            //     });
+            // };
 
             // to be run after google-search
             // TODO: Your search did not match any results
-            this.then (function () {
+            //
+            // NOTE: Browser-dependent selector
+            // The selector for the first link in Chrome is -
+            // //*[@id="rso"]/div[1]/div[1]/div/h3/a
+            // The selector for the first link in PhantomJS is -
+            // //*[@id="ires"]/ol/li[1]/h3/a
+
+            this.waitForSelector ( x('//*[@id="ires"]/ol/li[1]/h3/a'), function () {
                 console.log("Completed google search");
                 this.capture('google.png');
-                var url_list = this.evaluate(getLinks);
-                url_itunes = href2url(url_list[0]);
+
+                // var url_list = this.evaluate(getLinks);
+                // url_itunes = href2url(url_list[0]);
+
+                // url_itunes = this.getElementAttribute( x('/#<{(|[@id="ires"]/ol/li[1]/h3/a'), 'href' );
+                // url_itunes = href2url(url_itunes);
+                
+                // this.click(x('/#<{(|[@id="ires"]/ol/li[1]/h3/a'));
+                this.click('h3.r a');
             });
-            console.log("Obtained iTunes link");
 
         }
-
-        console.log("url_itunes : " + url_itunes);
-        console.log("Opening iTunes link");
-        // console.log("title : " + title);
-        // console.log("artist : " + artist );
-        // console.log("url_itunes : " + url_itunes);
-
-        this.thenOpen(url_itunes);
-        console.log("Opened iTunes link");
+        //
+        // this.thenOpen(url_itunes, function() {
+        //     console.log("Now opening url_itunes = " + url_itunes);
+        // });
 });
 
 // run after iTunes
-casper.then (function () {
-    // var textGenre = casper.fetchText(x('/#<{(|[@id="left-stack"]/div[1]/ul/li[2]/a[1]/span'));
-    this.capture('main.png');
-    var genre = casper.fetchText(x('//*[@id="left-stack"]/div[1]/ul/li[2]'));
+// CHROME xPath = 
+//                          //*[@id="left-stack"]/div[1]/ul/li[3]/span[2]
+casper.waitForSelector ( x('//*[@id="left-stack"]/div[1]/ul/li[3]/span[2]'), function () {
+
+    console.log("Loaded iTunes page");
+    this.capture('itunes.png');
+    var genre = casper.fetchText(x('//*[@id="left-stack"]/div[1]/ul/li[2]/a[1]/span'));
     var releaseDate = casper.fetchText(x('//*[@id="left-stack"]/div[1]/ul/li[3]/span[2]'));
     var album = casper.fetchText(x('//*[@id="title"]/div[1]/h1'));
     var img_url = casper.getElementAttribute(x('//*[@id="left-stack"]/div[1]/a[1]/div/img'), 'src');
     console.log("Results of iTunes :- ");
-    console.log("Genre : " + genre);
-    console.log("Date  : " + releaseDate);
-    console.log("Album : " + album);
-    console.log("img   : " + img_url);
-});
+    try {
+        console.log("Genre : " + genre);
+        console.log("Date  : " + releaseDate);
+        console.log("Album : " + album);
+        console.log("img   : " + img_url);
+    } catch (e) {
+        console.log("e : " + e);
+    }
+}, function () {
+    console.log("iTunes timed out after 10 seconds");
+    casper_print(this);
+    casper.capture("timeout_itunes.png");
+}, 10000);
 
 casper.run(function () {
     casper.exit();
