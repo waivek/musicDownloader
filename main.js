1df51e97c0ca680d2fed3f2c0e6f384884999fe3
waivek <waivek@gmail.com>
2015-10-13 18:38:08 +0530

    Precautionary commit

--- main.js
+++ main.js
@@ -5,18 +5,19 @@ var casper = require('casper').create();
 var x = require('casper').selectXPath;
 var url = "https://www.youtube.com/watch?v=U5K9AlmWM8s";
 var bad_url = "https://www.youtube.com/watch?v=I2REZSj4XnE";
+var title = "unknown_artist";
 
-casper.start(bad_url, function() {
-// casper.start(url, function() {
+// casper.start(bad_url, function() {
+casper.start(url, function() {
     this.echo(this.getTitle());
 }).viewport(1200, 1000);
 
 casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
-        var artist = "unknown_artist";
+        // var artist = "unknown_artist";
         var title = "unknown_title";
         var str = "unknown_str";
         var x_path_itunes = x('//*[@id="watch-description-extras"]/ul/li[1]/ul/li/a');
-        var url_itunes = this.getElementAttribute( x('//*[@id="watch-description-extras"]/ul/li[1]/ul/li/a'), 'href' );
+        var url_itunes = this.getElementAttribute( x_path_itunes, 'href' );
 
         // checking if YouTube has tagged the song and linked to the
         // corresponding iTunes page
@@ -31,7 +32,7 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
 
             str = casper.fetchText(x_path_itunes);
             title = extract_text( str, '"', '"');
-            artist = extract_text( str, '" by ', ' (');
+            // artist = extract_text( str, '" by ', ' (');
 
             this.thenOpen(url_itunes, function() {
                 console.log("Now opening url_itunes = " + url_itunes);
@@ -42,49 +43,55 @@ casper.waitForSelector(x('//*[@id="watch-description"]'), function () {
             // this means that YouTube has NOT tagged the song
             // we need to manually search for the iTunes link
 
-            url_itunes = "unknown_url";
-            str = casper.fetchText(x('//*[@id="eow-title"]'));
-
-            var getUrl = function (name) {
-                name = name.trim();
-                first = "https://www.google.co.in/search?q=";
-                mid = name.replace(/ /g, '+');
-                end = "+site%3Aitunes.apple.com";
-                return first + mid + end;
+            var str_clean = function ( str ) {
+                str = str.trim() + ' site:itunes.apple.com';
+                str = str.replace ( /lyric(s)?/ig, '' );
+                return str;
             };
+            url_itunes = "unknown_url";
+            str = this.fetchText(x('//*[@id="eow-title"]'));
+            str = str_clean ( str );
 
-            this.thenOpen(getUrl( str ), function () {
-                console.log("Now opening " + getUrl( str ));
+            this.thenOpen('http://google.fr/', function() {
+                this.fill('form[action="/search"]', { q: str}, true);
+                console.log('Googling "' + str + '"');
             });
+
             // TODO: Your search did not match any results
             this.waitForSelector ( 'h3.r a', function () {
                 this.click('h3.r a');
+                console.log("Loading iTunes page");
+            }, function () {
+                console.log("Google search timed out");
+                casper.capture("timeout_google.png");
             });
         }
+}, function () {
+    console.log("youtbe timed out");
+    casper.capture("timeout_youtube.png");  
 });
 
 casper.waitForSelector ( x('//*[@id="left-stack"]/div[1]/ul/li[3]/span[2]'), function () {
 
-    console.log("Loaded iTunes page");
+    this.echo("Loaded iTunes page" + this.getTitle());
     this.capture('itunes.png');
     var genre = casper.fetchText(x('//*[@id="left-stack"]/div[1]/ul/li[2]/a[1]/span'));
     var releaseDate = casper.fetchText(x('//*[@id="left-stack"]/div[1]/ul/li[3]/span[2]'));
     var album = casper.fetchText(x('//*[@id="title"]/div[1]/h1'));
     var img_url = casper.getElementAttribute(x('//*[@id="left-stack"]/div[1]/a[1]/div/img'), 'src');
+    var artist = casper.fetchText( x('//*[@id="title"]/div[1]/span/a/h2'));
     console.log("Results of iTunes :- ");
-    try {
-        console.log("Genre : " + genre);
-        console.log("Date  : " + releaseDate);
-        console.log("Album : " + album);
-        console.log("img   : " + img_url);
-    } catch (e) {
-        console.log("e : " + e);
-    }
+    console.log("artist: " + artist);
+    console.log("Genre : " + genre);
+    console.log("Date  : " + releaseDate);
+    console.log("Album : " + album);
+    console.log("img   : " + img_url);
+
 }, function () {
     console.log("iTunes timed out after 10 seconds");
     casper_print(this);
     casper.capture("timeout_itunes.png");
-}, 10000);
+});
 
 casper.run(function () {
     casper.exit();
