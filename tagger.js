612be4e1c6f3da300b0d95406f469dcf153f7f79
waivek <waivek@gmail.com>
2015-10-16 00:16:33 +0530

    First successful run of audio tagger
    There were a lot of problems in regards to framing the correct cmd
    arguments. This is due to JavaScripts bad String construction form.
    Certain problems may arise depending on where the album cover art is
    located.

--- tagger.js
+++ tagger.js
@@ -7,9 +7,15 @@ var add_quotes = function ( input ) {
 };
 var execSync = require('child_process').execSync;
 var tag = function ( input_path, input_file_name, title, album, artist, genre, date, img_path) {
-    var input = input_path + input_file_name;
-    var cmd_tag = 
-        "lame "  + add_quotes( input    ) + 
+    var dir_song = 'X:\\Dropbox\\js\\mp3\\music';
+    var input = input_file_name;
+    // var input = input_path + input_file_name;
+    var output = artist + " - " + title + ".mp3";
+    var dir_lame = "X:\\Dropbox\\js\\mp3\\";
+    var cmd_lame = dir_lame + "lame.exe ";
+
+    var cmd_tag =  
+        cmd_lame + add_quotes( input    ) + 
         " --tt " + add_quotes( title    ) +
         " --ta " + add_quotes( artist   ) +
         " --tg " + add_quotes( genre    ) +
@@ -17,21 +23,14 @@ var tag = function ( input_path, input_file_name, title, album, artist, genre, d
         " --ta " + add_quotes( artist   ) +
         " --ty " + add_quotes( date     ) +
         " --ti " + add_quotes( img_path ) +
-        " new_song.mp3";
-    var cmd_del = "del " + input;
-    var cmd_cd = "cd " + input_path;
-    var cmd_ren = "ren new_song.mp3 " + '"' + artist + " - " + title + ".mp3" + '"';
+        " "      + add_quotes( output   );
 
-    // console.log("cmd_tag : " + cmd_tag);
-    // console.log("cmd_del : " + cmd_del);
-    // console.log("cmd_ren : " + cmd_ren);
-    execSync( cmd_tag );
-    // console.log("cmd_cd : " + cmd_cd);
-    // execSync( cmd_del );
-    // execSync( cmd_ren );
+    var cmd_del = "del " + input_file_name;
 
-    // enter code here
+
+    execSync( cmd_tag, { cwd : dir_song } );
+    execSync( cmd_del, { cwd : dir_song } );
 };
 
 tag("music/", "song.mp3", "One Black Night", "REBOOT", "Wonder Girls",
-        "KPop", "2015", "music/img.jpg");
+        "KPop", "2015", "img.jpg");
