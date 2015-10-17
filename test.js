6e574df62e8df56e1983e1d1febc98f3bd080b95
waivek <waivek@gmail.com>
2015-10-17 13:52:05 +0530

    test has option parser/generator

--- test.js
+++ test.js
@@ -1,23 +1,37 @@
-var pretty_printer = function ( domain, string, time ) {
-    var len = 80;
-    var a = "[ " + domain + " ] ";
-    var b = string;
-    var c = " [ " + time + "s ]";
-    var len_rem = len - a.length - c.length;
-
-    var len_b = b.length;
-    var b_new = "unknown";
-    if ( b.length > len_rem ) {
-        var mid = len_rem / 2;
-        b_new = b.substring(0, mid-3) + "..." + b.substring(len_b - mid, len_b);
-    } else {
-        var len_space = len_rem - b.length;
-        b_new = b + Array( len_space+1 ).join(' ');
-    }
-    return a+b_new+c;
+var obj_to_string = function ( obj ) {
+    return JSON.stringify(obj, null, 4);
 };
 
-console.log("Formatted String :=\n" + pretty_printer('YouTube', 'message', 5));
-console.log("Formatted String :=\n" + pretty_printer('YouTube', 'a really long string that has no business being here and is probably a fucking URL for all I know but still must be accepted beause it has good parental connections fuck', 10));
+var option_to_string = function ( option, value ) {
+    var quote = '"';
+    var space = ' ';
+    return "--" + option + space + quote + value + quote;
+};
 
+var optional_arguments = function ( obj ) {
+    var key;
+    var res = [];
+    for ( key in obj ) {
+        var str = option_to_string( key, obj[ key ] );
+        res.push( str );
+    }
+    return res.join(' ');
+};
 
+var cmd_gen = function ( exe, options, input, output ) {
+    return [exe, options, input, output].join(' ');
+};
+
+var x = {
+    tt  : "One Black Night"    ,
+    ta  : "Wonder Girls"   ,
+    tg  : "KPop"    ,
+    tl  : "REBOOT"    ,
+    ty  : "2015"     ,
+    ti  : "img.jpg" 
+};
+
+// console.log("optional_arguments( x ).join('\n') : \n" + optional_arguments( x ).join('\n'));
+var str_opt = optional_arguments( x );
+var str_cmd = cmd_gen( "lame.exe", str_opt, "song.mp3", "new_song.mp3" );
+console.log("str_cmd : \n" + str_cmd);
