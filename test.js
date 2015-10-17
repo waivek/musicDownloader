fd7cfad55986f2e12b7153773b83b991905f5b1e
waivek <waivek@gmail.com>
2015-10-17 14:06:14 +0530

    Useless commit

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-var obj_to_string = function ( obj ) {
-    return JSON.stringify(obj, null, 4);
+module.exports = {
+    parser : function ( exe, obj_opt, input, output ) {
+        var str_opt = optional_arguments( x );
+        return 
+    }
 };
 
 var option_to_string = function ( option, value ) {
@@ -22,16 +25,14 @@ var cmd_gen = function ( exe, options, input, output ) {
     return [exe, options, input, output].join(' ');
 };
 
-var x = {
-    tt  : "One Black Night"    ,
-    ta  : "Wonder Girls"   ,
-    tg  : "KPop"    ,
-    tl  : "REBOOT"    ,
-    ty  : "2015"     ,
-    ti  : "img.jpg" 
-};
+// var x = {
+//     tt  : "One Black Night"    ,
+//     ta  : "Wonder Girls"   ,
+//     tg  : "KPop"    ,
+//     tl  : "REBOOT"    ,
+//     ty  : "2015"     ,
+//     ti  : "img.jpg" 
+// };
 
-// console.log("optional_arguments( x ).join('\n') : \n" + optional_arguments( x ).join('\n'));
-var str_opt = optional_arguments( x );
-var str_cmd = cmd_gen( "lame.exe", str_opt, "song.mp3", "new_song.mp3" );
-console.log("str_cmd : \n" + str_cmd);
+// var str_opt = optional_arguments( x );
+// console.log("str_cmd : \n" + str_cmd);
