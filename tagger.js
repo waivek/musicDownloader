22d4384a8c24a0b7fc1b2e6a048dcd8a8b7d19a6
waivek <waivek@gmail.com>
2015-10-19 18:03:40 +0530

    Precautionary Commit

--- tagger.js
+++ tagger.js
@@ -1,16 +1,24 @@
 var exec = require('child_process').exec;
 var parser = require('./parser').parser;
 var get_song_name = require('./helper').get_song_name;
+var hash_code = require('./helper').hash_code;
+var fs = require('fs');
 
-var o= {
-    "--tt"  : "One Black Night" ,
-    "--ta"  : "Wonder Girls"    ,
-    "--tg"  : "KPop"            ,
-    "--tl"  : "REBOOT"          ,
-    "--ty"  : "2015"            ,
-    "--ti"  : "img.jpg",
-    "--mp3input" : ""
-};
+
+var url = "https://www.youtube.com/watch?v=U5K9AlmWM8s";
+var bad_url = "https://www.youtube.com/watch?v=I2REZSj4XnE";
+var url_youtube = bad_url;
+
+// var o= {
+//     "--tt"  : "One Black Night" ,
+//     "--ta"  : "Wonder Girls"    ,
+//     "--tg"  : "KPop"            ,
+//     "--tl"  : "REBOOT"          ,
+//     "--ty"  : "2015"            ,
+//     "--ti"  : "img.jpg",
+//     "--mp3input" : ""
+// };
+
 var d_song = 'X:\\Dropbox\\js\\mp3\\music';
 var d_lame = "X:\\Dropbox\\js\\mp3\\";
 
@@ -18,9 +26,15 @@ var get_output_song_name = function ( obj ) {
     return obj["--ta"] + " - " + obj[ "--tt" ] + ".mp3";
 };
 
-var tag = function ( obj, dir_song, dir_lame ) {
+var tag = function ( url, dir_song, dir_lame ) {
 
-    var input = get_song_name( obj ) + ".tmp";
+    var hash = hash_code ( url ).toString();
+    var file_name = "X:\\Dropbox\\js\\mp3\\json\\" + hash + ".json";
+    var obj = JSON.parse(fs.readFileSync(file_name, 'utf8'));
+    obj[ "--ti" ] = "X:\\Dropbox\\js\\mp3\\covers\\" + hash + ".jpg";
+
+    // var input = get_song_name( obj ) + ".tmp";
+    var input = "X:\\Dropbox\\js\\mp3\\music\\" + hash + ".mp3";
     var output = get_song_name( obj ) + ".mp3";
 
     var exe = dir_lame + "lame.exe ";
@@ -38,4 +52,4 @@ var tag = function ( obj, dir_song, dir_lame ) {
 };
 
 // tag("song.mp3", o, d_song, d_lame );
-tag(o, d_song, d_lame );
+tag(url_youtube, d_song, d_lame );
