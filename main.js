beb529800bf8d85740fa6170f17ff0d0043bf8d8
waivek <waivek@gmail.com>
2015-10-27 02:31:47 +0530

    Trying to add playlist feature

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 var exec   = require('child_process').exec;
+var hash_code = require('./helper').hash_code;
+var fs = require( 'fs' );
 var parser = require('./parser').parser;
+var mkdirIfNoDir = require('./helper').mkdirIfNoDir;
 
 var tag = require('./tagger.js').tag;
 var download_song = require('./youtubedl.js').download_song;
 
-var get_url = function () {
+var playlist_to_array = function ( url_playlist ) {
+    return [ "https://www.youtube.com/watch?v=_KMyql7K98o",
+             "https://www.youtube.com/watch?v=tL_TFXbSnLY" ];
+};
+
+var get_url_array = function () {
+    return playlist_to_array( "whatever" );
 
     // [0] = exe [1] = file
     var url = process.argv[ 2 ];
-    return url;
+    return [ url ];
 };
 
 var target_dir = "X:\\Dropbox\\js\\mp3\\";
 var dir_lame      = "";
-var dir           = "X:\\Dropbox\\js\\mp3";
 
-var url = get_url();
-var fs = require( 'fs' );
-var hash_code = require('./helper').hash_code;
-var mkdirIfNoDir = require('./helper').mkdirIfNoDir;
-mkdirIfNoDir( target_dir + hash_code( url ).toString() );
-var cmd_csp = parser( "casperjs.bat", { "crawler.js" : "" }, url, "" );
+var main = function ( url ) {
+    mkdirIfNoDir( target_dir + hash_code( url ).toString() );
+    var cmd_csp = parser( "casperjs.bat", { "crawler.js" : "" }, url, "" );
 
-var opt = { cwd : dir };
-var t = function () {
-    return tag( url );
+    var opt = { cwd : target_dir };
+    var t = function () {
+        return tag( url );
+    };
+
+    var eventEmitter = exec( cmd_csp, opt, function ( err, stdout, stderr ) {
+        if ( err !== null ) { console.log("err : " + err); }
+        download_song( url, t );
+    } );
+
+    eventEmitter.stdout.on( 'data', function ( data ) {
+        process.stdout.write( data );
+    });
+
 };
 
-var eventEmitter = exec( cmd_csp, opt, function ( err, stdout, stderr ) {
-    if ( err !== null ) { console.log("err : " + err); }
-    download_song( url, t );
-} );
-
-eventEmitter.stdout.on( 'data', function ( data ) {
-    process.stdout.write( data );
-});
-
+var array_url = get_url_array();
+array_url.map( main );
--- test.js
+++ test.js
@@ -1,4 +1,37 @@
-// var tag = require('./tagger.js').tag;
-// tag( "https://www.youtube.com/watch?v=_KMyql7K98o" );
-var fs = require( 'fs' );
-var dir = "X:\\Dropbox\\js\\mp3\\-1901684268"
+var casper_print = require('./helper').casper_print;
+var extract_text = require('./helper').extract_text;
+var casper = require('casper').create();
+var x = require('casper').selectXPath;
+
+var url = "https://www.youtube.com/playlist?list=PLabEsp9mKEJJmv-h3iBx7ZWe4_MAYDtoY";
+casper.start(url, function() {
+    this.echo("\nOpened " + this.getTitle());
+}).viewport(1200, 1000);
+
+var get_href = function ( anchor ) {
+    return anchor.getAttribute( 'href' );
+};
+casper.waitForSelector( x( '//*[@id="pl-load-more-destination"]/tr[1]/td[4]/a' ), function () {
+    try {
+        // var view_text = this.fetchText( x( '/#<{(|[@id="pl-header"]/div[2]/ul/li[2]' ) );
+        // var size = parseInt( x );
+        var table = document.querySelector( '#pl-load-more-destination' );
+        var obj_to_string = require('./helper').obj_to_string;
+        console.log("obj_to_string( table ) : " + obj_to_string( table ));
+        this.capture('images/pic.png');
+        var array_links = table.querySelectorAll( 'a' );
+        var x = array_links.map( get_href );
+        console.log("x : " + x);
+    } catch ( e ) {
+        console.log("e : " + e);
+    }
+    
+}, function () {
+    this.echo("timeout_playlist_to_url.png");
+    casper_print(this);
+    this.capture('images/timeout_playlist_to_url.png');
+});
+
+casper.run(function () {
+    casper.exit();
+});
